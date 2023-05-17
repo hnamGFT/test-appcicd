06e346b73d75c7614b0a329e849d104f5a3ca7f4
hnamGFT <129087039+hnamGFT@users.noreply.github.com>
2023-05-17 22:18:59 +0100

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to GFT.";
+		return "welcome to Ideabank";
 	}
 
 	public static void main(String[] args) {
